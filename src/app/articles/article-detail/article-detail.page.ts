0994707876ba8c25eb2659a4112891d0d2669e06
Yannick <huyiyan1986@gmail.com>
2025-02-12 15:01:38 +0100

    added error handle in article detail page

--- src/app/articles/article-detail/article-detail.page.ts
+++ src/app/articles/article-detail/article-detail.page.ts
@@ -1,6 +1,6 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, computed, effect, inject } from '@angular/core';
 import { rxResource } from "@angular/core/rxjs-interop";
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton } from '@ionic/angular/standalone';
 import { ArticleService } from 'src/app/services/article.service';
 
@@ -18,8 +18,16 @@ export class ArticleDetailPage {
     request: () => ({ articleId: this.activatedRoute.snapshot.params['articleId'] }),
     loader: ({ request }) => this.articleService.getArticle(request.articleId)
   });
+  private readonly router = inject(Router);
 
   article = computed(() => this.articleResource.value());
+  error = this.articleResource.error;
+
+  constructor() {
+    effect(() => {
+      if (this.error()) this.router.navigate(['/articles']);
+    })
+  }
 }
 
 
